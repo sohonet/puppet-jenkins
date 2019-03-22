c45bc14e3fb5bc6e8aed72219363b656d321b877
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2019-03-22 15:37:36 +0000

    fix typo

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -25,7 +25,7 @@ class jenkins::repo::debian
       location   => 'http://pkg.jenkins-ci.org/debian',
       release    => 'binary/',
       repos      => '',
-      key        =>
+      key        => {
         'id'     => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
         'source' => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
       }
